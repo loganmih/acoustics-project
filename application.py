8fc441bfafd3adbe1c44f282043cd5fbd0fc8a99
loganmih <loganm.hewitt@gmail.com>
2023-12-03 10:11:20 -0500

    Added file icon and file text after upload.
    Reorganized model, view, and controller system

--- application.py
+++ application.py
@@ -1,55 +1,21 @@
 from tkinter import ttk
 from tkinter import font as tkFont
-from tkinter import filedialog as fd
 import tkinter as tk
 from PIL import ImageTk, Image
-import os, io
-# Create model, view, and controller objects in order to organize funcionality in the application.
-# Creating the model, view, and controller objects here will reduce clutter in the project.
+from model import Model
+
+
+# Create view, and controller objects in order to organize funcionality in the application.
+# Creating the  view, and controller objects here will reduce clutter in the project.
 # The other py modules will contain code to handle the audio processing.
 
 root = tk.Tk()
 
 helveticaLabel = tkFont.Font(family='Helvetica', size=18, weight='bold')
+helveticaText = tkFont.Font(family='Helvetica', size=16)
 helveticaButton = tkFont.Font(family='Helvetica', size=12, weight='bold')
 
-class Model():
-
-    #initialize object variables
-    def __init__(self):
-        self.filename = ""
-        self.f: io.TextIOWrapper
-    
-    #get file from user and open it
-    def openFile(self):
-        filetypes = (
-            ('Text files', '*.txt'),
-            ('All files', '*.*')
-        )
-
-        self.filename = fd.askopenfilename(
-            title='Open a file',
-            initialdir='/',
-            filetypes=filetypes
-        )
-
-        self.f = open(self.filename, 'r')
-    
-    #these are the functions that will calculate the values such as the Waveform, resonance,
-    #low, med, high rt60, etc
-    def getResonance(self):
-        pass
-
-    def getRT(self):
-        pass
-
-    def __del__(self):
-        self.f.close()
-    
 class MainView(ttk.Frame):
-
-    def fileUploaded(self):
-        self.panel.grid(row=2, column=0, sticky='W', pady=20)
 
     def uploadButtonClicked(self):
         self.controller.upload()
@@ -57,28 +23,51 @@ class MainView(ttk.Frame):
     def setController(self, controller):
         self.controller = controller
 
+    
+    #code to run once a file has been uploaded
+    def fileUploaded(self):
+        self.panel.pack(side=tk.LEFT)
+        
+        #ensure the right file name is being used for the label
+        self.fileLabel.config(text=self.fileName, justify=tk.RIGHT)
+        self.fileLengthLabel.config(text=self.controller.getWavLength(), justify=tk.RIGHT)
+
+        self.fileLabel.pack(side=tk.TOP)
+        self.fileLengthLabel.pack(side=tk.RIGHT, expand=True, fill=tk.Y)
+
+        self.fileText.grid(row=1, column=0, sticky=tk.W, pady=20)
+
+    
     def __init__(self, parent: tk.Tk):
         super().__init__(parent)
 
         self.controller: Controller()
+        self.fileName = ""
 
         #set size of root window and prevent resizing
         parent.resizable(False, False)
         parent.geometry('700x400')
+        parent.config(padx=20, pady=20)
+
+        self.fileText = tk.Frame(parent)
+        self.uploadText = tk.Frame(parent)
 
         #Upload File label and button
-        self.uploadLabel = ttk.Label(self, text="Upload Data", font=helveticaLabel)
-        self.uploadLabel.grid(row=1, column=0)
+        self.uploadLabel = ttk.Label(self.uploadText, text="Upload Data", font=helveticaLabel)
+        self.uploadLabel.pack(side=tk.LEFT, fill=tk.X)
+        self.uploadButton = tk.Button(self.uploadText, text="Select File", font=helveticaButton, command=self.uploadButtonClicked)
+        self.uploadButton.pack(side=tk.RIGHT, fill=tk.X)
 
-        self.uploadButton = tk.Button(self, text="Select File", font=helveticaButton, command=self.uploadButtonClicked)
-        self.uploadButton.grid(row=1, column=1)
+        self.uploadText.grid(row=0, column=0, sticky="w")
 
         #every widget after file upload, such as convert status, plot button, file icon, time, waveform,
         #highest resonance, low, medium, and high frequency RT60, etc
         #once file is uploaded, these widgets will show
-
         self.fileIcon = ImageTk.PhotoImage(Image.open('wavicon.png').resize((60,60)))
-        self.panel = tk.Label(self, image=self.fileIcon)
+        self.panel = tk.Label(self.fileText, image=self.fileIcon)
+
+        self.fileLabel = ttk.Label(self.fileText, font=helveticaText)
+        self.fileLengthLabel = ttk.Label(self.fileText, font=helveticaText)
         
 class Controller():
 
@@ -86,15 +75,20 @@ class Controller():
         self.model = model 
         self.view = view
 
-
     def upload(self):
         self.model.openFile()
+
+        #grab file name before indicating it has been uploaded
+        self.view.fileName = self.model.getFileName()
         self.view.fileUploaded()
+
+    def getWavLength(self):
+        return self.model.getWavLength()
 
 view = MainView(root)
 model = Model()
 controller = Controller(model, view)
 view.setController(controller)
-view.grid(row=0, column=0, padx=20, pady=20)
+view.grid(row=0, column=0)
 
 view.mainloop()
---
+++ model.py
@@ -0,0 +1,42 @@
+import os, io
+from tkinter import filedialog as fd
+
+class Model():
+
+    #initialize object variables
+    def __init__(self):
+        self.filename = ""
+        self.f: io.TextIOWrapper
+    
+    #get file from user and open it
+    def openFile(self):
+        filetypes = (
+            ('Text files', '*.txt'),
+            ('All files', '*.*')
+        )
+
+        self.filename = fd.askopenfilename(
+            title='Open a file',
+            initialdir='/',
+            filetypes=filetypes
+        )
+
+        self.f = open(self.filename, 'r')
+    
+    #these are the functions that will calculate the values such as the Waveform, resonance,
+    #low, med, high rt60, etc
+    def getResonance(self):
+        pass
+
+    def getRT(self):
+        pass
+
+    def getWavLength(self):
+        return 5 
+
+    def getFileName(self):
+        print(self.filename)
+        return self.filename
+
+    def __del__(self):
+        self.f.close()
